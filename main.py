6593317bb0910c63fa2efff066dcd073a069c53b
Tomás vidal <tomi_vidal_99@hotmail.com>
2019-07-29 10:31:15 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from appJar import gui
+import tkinter as tk
+
+# handle buttons pressin'
+def press(button):
+    if (button == 'Exit'):
+        app.stop()
+    else:
+        user_key = app.getEntry("User key")
+        download_path = app.getEntry("Images path")
+        print("key:", user_key, "download path:", download_path)
+
+
+# basic configuration of app
+app = gui('Bulk from pexels', '640x480')
+app.configure(bg='black', fg='white', font={'size':14, 'family': 'Helvica'}, resizable='True')
+app.buttonFont = 10
+app.setTransparency(100)
+app.setLocation('CENTER')
+
+# define labels and buttons
+app.addLabel('title', 'Download all the images from PEXELS! :)')
+app.addLabelEntry('User key')
+app.addLabelEntry('Images path')
+app.addButtons(["Accept", "Exit"], press)
+
+# run app
+app.go()
