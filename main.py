80056832407c4d701720d7b89c5fc151c1057c21
karimkohel <kareemkohel@gmail.com>
2019-07-30 20:32:52 +0200

    re-organized code

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+"""
+								dPexel  0.5
+A simple app with a UI that allows users to download bulk images from Pexel (image wesbsite)
+
+"""
+
+############### Imports ###############
+
 from appJar import gui
-import tkinter as tk
+
+############### FX ###############
+
 
 # handle buttons pressin'
 def press(button):
@@ -12,19 +22,20 @@ def press(button):
 	else:
 		pass
 
+############### Config ###############
 
-# basic configuration of app
 app = gui('Bulk from pexels', '640x480')
 app.configure(bg='lightgray', fg='black', font={'size':14, 'family': 'Helvica'}, resizable='True')
 app.buttonFont = 10
 app.setTransparency(100)
 app.setLocation('CENTER')
 
-# define labels and buttons
+############### Main ###############
+
 app.addLabel('title', 'Download all the images from PEXELS! :)')
 app.addLabelEntry('User key')
 app.addLabelEntry('Images path')
 app.addButtons(["Accept", "Exit"], press)
 
-# run app
+
 app.go()
