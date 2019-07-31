2f40be8ced8b36bb2ce0dd6f19bdd18dbae21d88
karimkohel <kareemkohel@gmail.com>
2019-07-31 12:00:14 +0200

    added scale for users to input how many photos to download

--- gui.py
+++ gui.py
@@ -20,6 +20,7 @@ def accept():
 
 def download():
 	# function that executes the searching, gets variables from inputs
+	num_of_photos = app.getScale("Number of photos")
 	execute_search(app.getEntry('User key:'), app.getEntry('Searching word:'), constant.search_pages)
 
 ############### Define GUI components ##################
@@ -36,19 +37,26 @@ app.addLabel('description', 'You can find your key in this link',colspan=3)
 app.addLabel('description_link', 'https://www.pexels.com/api/new/',colspan=3)
 app.addLabelEntry('Searching word:',colspan=3)
 app.addLabelEntry('Path',colspan=3)
-app.addButton('Accept', accept,6,0)
-app.addButton('Download',download,6,1)
-app.addButton('Exit',app.stop,6,2)
+app.addLabelScale("Number of photos",colspan=3)
+app.addButton('Accept', accept,7,0)
+app.addButton('Download',download,7,1)
+app.addButton('Exit',app.stop,7,2)
+
 
 
 
 ############### Config ################################
+
+minimum_photos = 0
+max_photos = 100
 
 app.configure(bg='lightgray', fg='black', font={'size':14, 'family': 'Helvica'}, resizable='True')
 app.buttonFont = 10
 app.setTransparency(100)  #Transparency isnt working apprently
 app.setLocation('CENTER')
 app.setLabelFg('description_link', 'blue')
+app.setScaleRange("Number of photos", minimum_photos, max_photos, curr=10)
+app.showScaleValue("Number of photos", show=True)
 
 ############## Run app #################################
 
